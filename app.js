9485be3f59243552f00212f67d07a05f58dd776b
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2017-01-26 11:36:26 -0800

    Further game functionality

--- app.js
+++ app.js
@@ -13,20 +13,43 @@ var Game = function() {
   this.prompt = require('inquirer').prompt;
 }
 
+// Returns false for no win, 1 for player 1, 2 for player 2
 Game.prototype.checkForWin = function () {
+  // Check for diagonals
+  if ((this.board[0][0] == 'X' && this.board[1][1] == 'X' && this.board[2][2] == 'X') || (this.board[0][2] == 'X' && this.board[1][1] == 'X' && this.board[2][0] == 'X')) {
+    return 1;
+  }
+  if ((this.board[0][0] == 'O' && this.board[1][1] == 'O' && this.board[2][2] == 'O') || (this.board[0][2] == 'O' && this.board[1][1] == 'O' && this.board[2][0] == 'O')) {
+    return 2;
+  }
+  // check for rows and columns
+  for (var i = 0; i < 3; i++) {
+    if ((this.board[0][i] == 'O' && this.board[1][i] == 'O' && this.board[2][i] == 'O') || (this.board[i][0] == 'O' && this.board[i][1] == 'O' && this.board[i][2] == 'O')) {
+      return 1;
+    }
+    if ((this.board[0][i] == 'O' && this.board[1][i] == 'O' && this.board[2][i] == 'O') || (this.board[i][0] == 'O' && this.board[i][1] == 'O' && this.board[i][2] == 'O')) {
+      return 2;
+    }
+  }
+  return false;
 }
 
 Game.prototype.drawBoard = function () {
-  var table = new Table({head: ["", "Column 0", "Column 1", "Column 2"] });
+  var table = new Table({head: [Math.random(), "Column 0", "Column 1", "Column 2"] });
   table.push(
     { 'Row 0': this.board[0] },
     { 'Row 1': this.board[1] },
     { 'Row 2': this.board[2] }
   );
-  return table.toString();
+
+  var tableString = table.toString();
+  //return tableString;
+  return JSON.stringify(this.board);
 }
 
-Game.prototype.getPlayerInput = function () {
+Game.prototype.newRound = function () {
+  console.log(this.drawBoard());
+  var self = this;
   this.prompt([{
     type: 'input',
     name: 'row',
@@ -37,11 +60,11 @@ Game.prototype.getPlayerInput = function () {
     name: 'column',
     message: 'Player ' + this.currentPlayer + ', it\'s your turn! Please enter a column (0,1,2)'
   }]).then(function (answers) {
-    this.processAnswer(answers.row, answers.column);
+    self.processAnswer(answers.row, answers.column);
   });
 }
 
-Game.prototype.processAnswer(row, column) {
+Game.prototype.processAnswer = function(row, column) {
   var char;
   if (this.currentPlayer === 1) {
     char = 'X';
@@ -49,15 +72,27 @@ Game.prototype.processAnswer(row, column) {
   else if (this.currentPlayer === 2) {
     char = 'O';
   }
-  this.board[row, column] = char
+  if (row >= 0 && column >= 0 && row <= 2 && column <=2 && this.board[row][column] === ' ') {
+    this.board[row][column] = char
+    if (this.currentPlayer === 1) {
+      this.currentPlayer = 2;
+    } else {
+      this.currentPlayer = 1;
+    }
+  } else {
+    vorpal.log('Invalid move!');
+  }
+  if (this.checkForWin()) {
+    vorpal.log('Player ' + this.currentPlayer + 'wins!'); 
+    process.exit();
+  } else {
+    // Continue game
+    this.newRound();
+  }
 }
 
 Game.prototype.start = function () {
-  vorpal
-    .delimiter('tictactoe$')
-    .show();
-  vorpal.log(this.drawBoard());
-  this.getPlayerInput();
+  this.newRound();
 }
 
 var game = new Game();
